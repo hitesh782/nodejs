f466dbca9ea7af73731e85b8894b1c0c82f23ad1
Hitesh <hiteshparmar782002@gmail.com>
2023-07-29 18:23:48 +0530

    added response header and status

--- routed-server.js
+++ routed-server.js
@@ -2,7 +2,10 @@ const http = require('http');
 
 const fs = require("fs");
 
-const html = fs.readFileSync('./template/index.html', 'utf-8');
+// const html = fs.readFileSync('./template/index.html', 'utf-8');
+const html = fs.readFileSync('./template/index2.html', 'utf-8');
+
+let products = JSON.parse(fs.readFileSync('./data/product.json', 'utf-8'));
 
 //1 : creating a server
 const server = http.createServer((req, res) => {
@@ -11,14 +14,41 @@ const server = http.createServer((req, res) => {
 
     if (path === '/' || path.toLocaleLowerCase() === '/home') {
         // res.end('you are on home page');
-        res.end(html);
+
+        //for setting the header
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'my-Header': 'hello world'
+        });
+        // res.end(html);
+        res.end(html.replace('{{%CONTENT%}}', 'You are in Home page'))
     }
 
     else if (path.toLocaleLowerCase() === '/about') {
         // res.end('you are on about page');
-        res.end(html);
+        res.writeHead(200);
+        // res.end(html);
+        res.end(html.replace('{{%CONTENT%}}', 'You are in About page'))
+    }
+    else if (path.toLocaleLowerCase() === '/contact') {
+        // res.end('you are on about page');
+        res.writeHead(200);
+        // res.end(html);
+        res.end(html.replace('{{%CONTENT%}}', 'You are in Contact page'))
+    }
+    else if (path.toLocaleLowerCase() === '/products') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        // fs.readFile('./data/product.json', 'utf-8', (error, data) => {
+        //     let products = JSON.parse(data);
+        //     res.end(data)
+        // })
+        res.end('products page');
+        console.log(products);
     }
     else {
+        res.writeHead(404);
         res.end('there is no path match');
     }
 
