8c2fb45da47d2e3bcf8d87078f19361aa76edfbc
Hitesh <hiteshparmar782002@gmail.com>
2023-08-03 22:51:12 +0530

    connected mongodb database to the app

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './config.env' });
 
 const app = require('./routes-tut');  // put app after dotenv config
@@ -10,6 +9,16 @@ const app = require('./routes-tut');  // put app after dotenv config
 // console.log(app.get('env'));  //development (default environment)
 
 console.log(process.env); //print all environment variables
+
+//connecting to the database
+mongoose.connect(process.env.CONN_STR, {
+    useNewUrlParser: true
+}).then((conn) => {
+    console.log(conn);
+    console.log('DB connected succesfully');
+}).catch((err) => {
+    console.log('some error has occured when connecting to the database');
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
